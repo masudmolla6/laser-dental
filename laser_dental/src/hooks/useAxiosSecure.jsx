56386313db3c4c718ace1dcf2612b30fe089aa70
masudmolla6 <masudmolla2937@gmail.com>
2026-06-27 22:52:58 +0600

    updated useaaxiosSecure

--- laser_dental/src/hooks/useAxiosSecure.jsx
+++ laser_dental/src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 
 // ── axios instance ─────────────────────────────────────────────────────────
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 // ✅ REQUEST interceptor — useEffect এর বাইরে
